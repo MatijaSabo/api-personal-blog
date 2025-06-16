6c092d353789c5f793781d3eb508689247b62502
Matija Sabolić <matija.sabolic@ibm.com>
2025-06-16 10:31:27 +0200

    Adding custom home page and 404 page

--- server.js
+++ server.js
@@ -8,9 +8,20 @@ const bodyParser = require('body-parser')
 const createServer = async () => {
     app.use(cors())
     app.use(bodyParser.json())
+    app.use(express.static('./public'))
 
     // routes
     require(`./src/routes/api`)(app);
+
+    // home page
+    app.get('/', function(req, res) {
+        res.sendFile('./public/templates/index.html', { root: __dirname });
+    });
+
+    // error page
+    app.get(/(.*)/, function(req, res) {
+        res.status(404).sendFile('./public/templates/error.html', { root: __dirname });
+    });
 
     app.listen(port, () => {
         console.log(`App listening at http://localhost:${port}`)
