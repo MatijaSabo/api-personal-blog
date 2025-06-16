1bcdce7587ef02814538e0157cfef40fbb1579a3
Matija Sabolić <matija.sabolic@ibm.com>
2025-06-16 09:36:45 +0200

    Adding cache controll headers

--- src/modules/feedbacks/controller/controller.js
+++ src/modules/feedbacks/controller/controller.js
@@ -1,16 +1,17 @@
 const FeedbackService = require(`../service/service`);
-
 class FeedbackController {
 
     async findAll(req, res) {
         const data = await FeedbackService.findAll()
 
+        res.setHeader('Cache-Control', 'max-age=600, stale-while-revalidate=60')
         res.json(data)
     }
 
     async findByID(req, res) {
         const data = await FeedbackService.findByID(req.params.id)
 
+        res.setHeader('Cache-Control', 'max-age=1800, stale-while-revalidate=60')
         res.json(data)
     }
 
